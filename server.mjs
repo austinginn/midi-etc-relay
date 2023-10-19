a0fa08927e4f95bcdd7f7e9dff42d1cd7fec1f87
Austin Ginn <austinleeginn@gmail.com>
2023-10-19 11:48:35 -0400

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -3,7 +3,7 @@ import UDPClient from './UDPClient.mjs';
 const port = 4703;
 const ip = "";
 
-const etc = new UDPClient("", 4703);
+const etc = new UDPClient("172.22.0.59", 4703);
 
 const message = 'E$pst act: 1, 1, 0\r';
 
