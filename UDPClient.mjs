5cd7d0b2b807aa454f454c0192f724e658a6fa06
Austin Ginn <austinleeginn@gmail.com>
2023-10-19 15:23:35 +0000

    first commit

---
+++ UDPClient.mjs
@@ -0,0 +1,26 @@
+import dgram from 'dgram';
+
+class UDPClient {
+    constructor(ip, port) {
+        this.ip = ip;
+        this.port = port;
+        this.client = dgram.createSocket('udp4');
+    }
+
+    sendMessage(message) {
+        const buffer = Buffer.from(message);
+        this.client.send(buffer, 0, buffer.length, this.port, this.ip, (err) => {
+            if (err) {
+                console.error(err);
+            } else {
+                console.log(`Message sent to ${this.ip}:${this.port}: ${message}`);
+            }
+        });
+    }
+
+    close() {
+        this.client.close();
+    }
+}
+
+export default UDPClient;
---
+++ server.mjs
@@ -0,0 +1,10 @@
+import { UDPClient } from './UDPClient.mjs';
+
+const port = 4703;
+const ip = "";
+
+const etc = new UDPClient("", 4703);
+
+const message = 'E$pst act: 1, 1, 0\r';
+
+etc.send(message);
