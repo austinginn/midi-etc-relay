0eaa2e3b2b05d3d62503ec24c0027d43e15cb356
austinginn <austinginn@gmail.com>
2023-10-24 15:00:31 -0400

    hopefully got this right

--- server.mjs
+++ server.mjs
@@ -1,10 +1,39 @@
 import UDPClient from './UDPClient.mjs';
+import midi from 'midi';
 
-const port = 4703;
+const etc_port = 4703;
 const ip = "";
 
-const etc = new UDPClient("172.22.0.59", 4703);
+const etc = new UDPClient("172.22.0.59", etc_port);
 
-const message = 'E$pst act: 1, 1, 0\r';
+//set up midi input
+const input = new midi.Input();
 
-etc.send(message);
+//log total available input ports
+const avail_ports_count = input.getPortCount();
+console.log(avail_ports_count);
+
+//itterate through available ports and log names
+for(let i = 0; i <= avail_ports_count; i++){
+	console.log(i, input.getPortName);
+}
+
+//callback
+input.on('message', (deltaTime, message) => {
+	console.log(`msg: ${message} delta: ${deltaTime}`);
+)};
+
+
+//open port if available
+
+//
+
+
+
+
+
+
+
+//const message = 'E$pst act: 1, 1, 0\r';
+
+//etc.send(message);
