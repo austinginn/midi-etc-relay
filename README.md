2c4f9527b2532d12d882b181e557c2455054e3ab
Austin Ginn <austinleeginn@gmail.com>
2023-10-25 02:56:28 +0000

    ready for testing

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # etc-udp-control
-UPD control for ETC Echo Touch MK2
+Midi to etc-udp relay
--- server.mjs
+++ server.mjs
@@ -1,46 +1,49 @@
 import UDPClient from './UDPClient.mjs';
 import midi from 'midi';
 
+//etc
 const etc_port = 4703;
 const etc_ip = "192.168.40.101";
-const midi_port_name = "U2MIDI Pro";
-const push_midi_channel = 159 // ch.16
-const trans_midi_channel = 158 // ch.15
-
 const etc = new UDPClient(etc_ip, etc_port);
+
+//midi
+const midi_port_name = "U2MIDI Pro";
+const trans_midi_channel = 158; // ch.15
+const push_midi_channel = 159; // ch.16
 
 //set up midi input
 const input = new midi.Input();
 
 //get midi port index that matches name
 const index = getMidiPortIndex(midi_port_name);
-console.log(index);
-if(index == -1){ console.log("midi port not found"); process.exit();};
+if (index == -1) { console.log("midi port not found"); process.exit(); };
 
-//callback
+//callback for midi input
 input.on('message', (deltaTime, message) => {
 	console.log(`msg: ${message} delta: ${deltaTime}`);
-	if(message[0] == push_midi_channel && message[2] == 127){
-		
-		etc.send(`E$pst act: 1,${message[1]}, 0\r`);
-return;
+
+	//midi channel with support for fade up and down times
+	//midi ch.15, note = preset number, velocity = fade up time in seconds (maxed at 29 seconds)
+	if (message[0] == trans_midi_channel) {
+		etc.send(`E$pst act: 1,${message[1]}, ${message[2]}\r`);
+		return;
 	}
-
-if(message[0] == trans_midi_channel){
-etc.send(`E$pst act: 1,${message[1]}, ${message[2]}\r`);
-return;
+	
+	//midi channel instant preset load
+	if (message[0] == push_midi_channel && message[2] == 127) {
+		etc.send(`E$pst act: 1,${message[1]}, 0\r`);
+		return;
+	}
 });
-
 
 //open midi port
 input.openPort(index);
 
-
 //get midi port index
-function getMidiPortIndex(name){
+function getMidiPortIndex(name) {
 	const avail_ports_count = input.getPortCount();
-	for(let i = 0; i < avail_ports_count; i++){
-		if(input.getPortName(i).includes(name)){
+	for (let i = 0; i < avail_ports_count; i++) {
+		if (input.getPortName(i).includes(name)) {
 			return i;
 		}
 	}
