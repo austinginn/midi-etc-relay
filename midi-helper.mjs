f635fd6a3227ed9d989bc17546d66efb803c61fd
Austin Ginn <austinleeginn@gmail.com>
2023-10-25 14:38:00 +0000

    midi helper file

---
+++ midi-helper.mjs
@@ -0,0 +1,13 @@
+//prints available midi ports
+import midi from 'midi';
+
+const input = new midi.Input();
+
+//print available midi ports:
+console.log("Available midi ports:");
+console.log("*****************");
+const avail_ports_count = input.getPortCount();
+for (let i = 0; i < avail_ports_count; i++) {
+    console.log(input.getPortName(i));
+}
+console.log("*****************");
